b782e8fb7e9958eabb6a4e7f5242fd733ee40276
RST20242024 <raquel.soguero@devoteam.com>
2024-10-09 17:17:23 +0200

    Update streamlit_app.py
    Añadir cambios

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Smoothie de Raquel:cup_with_straw:")
@@ -16,7 +16,9 @@ st.write(
 name_on_order = st.text_input("Nombre del Smoothie:")
 st.write("El nombre de tu smoothie será:", name_on_order)
 
-session = get_active_session ()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table ("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data = my_dataframe, use_container_width=True)
 
